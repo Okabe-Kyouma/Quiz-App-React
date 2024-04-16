f313d1689eedb8cd16008e56f128b3dc3b100f25
Okabe-Kyouma <ayushpal5432@gmail.com>
2024-04-17 02:50:20 +0530

    ReactQuizApp

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,18 @@
+const Footer = () => {
+  return (
+    <div
+      style={{
+        textAlign: "center",
+        marginBottom: 10,
+      }}
+    >
+      <a
+        href="https://www.youtube.com/roadsidecoder"
+        style={{ cursor: "pointer" }}
+      >
+      </a>
+    </div>
+  );
+};
+
+export default Footer;
